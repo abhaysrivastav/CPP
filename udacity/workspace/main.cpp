c28544c1c44d0ce99acb30bd531ae4d88c4e8047
abhaysrivastav <abhaysrivastav@users.noreply.github.com>
2020-09-06 20:24:27 +0530

    Read the Board from file Example

---
+++ udacity/workspace/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+using std::cout;
+using std::string;
+using std::vector;
+using std::ifstream;
+
+// TODO: Add the ReadBoardFile function here.
+void ReadBoardFile(string path){
+	ifstream myfile(path);
+  	if(myfile)
+    {
+    	string line;
+      	while(getline(myfile, line))
+        {
+        	cout << line << "\n";
+        }
+    }
+
+}
+
+// PrintBoard not used in this exercise
+void PrintBoard(const vector<vector<int>> board) {
+  for (int i = 0; i < board.size(); i++) {
+    for (int j = 0; j < board[i].size(); j++) {
+      cout << board[i][j];
+    }
+    cout << "\n";
+  }
+}
+
+int main() {
+  // TODO: Call the ReadBoardFile function here.
+  ReadBoardFile("1.board");
+  
+  // Leave the following line commented out.
+  //PrintBoard(board);
+}
