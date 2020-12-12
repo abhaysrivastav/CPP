432b03451dac2113c0c5fc2e282cfc153dbf0c92
abhaysrivastav <abhaysrivastav@users.noreply.github.com>
2020-12-12 18:35:43 +0530

    Delete main.cpp

--- CppND-System-Monitor/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ncurses_display.h"
-#include "system.h"
-#include <iostream>
-int main() {
-  System system;
-  int process_to_display = 30;
-  NCursesDisplay::Display(system, process_to_display);
-}
